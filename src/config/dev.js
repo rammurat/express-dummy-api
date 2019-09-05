134f6637391abc25a38e51399a66f683a058cb3a
Ram Murat Sharma <https://ramsharm@tools.publicis.sapient.com>
2019-09-05 15:29:41 +0100

    Initial setup

---
+++ src/config/dev.js
@@ -0,0 +1,6 @@
+export const config = {
+  secrets: {
+    jwt: 'jwt'
+  },
+  dbUrl: 'mongodb://localhost:27017/api-design'
+}
---
+++ src/server.js
@@ -0,0 +1,44 @@
+import express from 'express'
+import { json, urlencoded } from 'body-parser'
+import morgan from 'morgan'
+import config from './config'
+import cors from 'cors'
+import { connect } from './utils/db'
+import itemRouter from './resources/item/item.router'
+import listRouter from './resources/list/list.router'
+
+export const app = express()
+
+app.disable('x-powered-by')
+
+app.use(cors())
+app.use(json())
+app.use(urlencoded({ extended: true }))
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.send({ message: 'hiiiii' })
+})
+
+app.get('/data', (req, res) => {
+  res.send({ message: 'hiiiii' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'hiiiii' })
+})
+
+app.use('/item', itemRouter)
+app.use('/list', listRouter)
+
+export const start = async () => {
+  try {
+    await connect()
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}`)
+    })
+  } catch (e) {
+    console.error(e)
+  }
+}
